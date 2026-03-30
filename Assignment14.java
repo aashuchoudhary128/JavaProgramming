fe71b559c45605bf57f91a8cd1d460a27bf45f0a
Aashu Choudhary <aashuchoudhary128@gmail.com>
2026-03-30 19:23:10 +0530

    Add files via upload

---
+++ Assignment14.java
@@ -0,0 +1,36 @@
+class Person {
+    void role() {
+        System.out.println("I am a person.");
+    }
+}
+
+class Employee extends Person {
+    @Override
+    void role() {
+        System.out.println("I am an employee.");
+    }
+}
+
+class Manager extends Employee {
+    @Override
+    void role() {
+        System.out.println("I am a manager.");
+    }
+}
+
+
+public class Assignment14 {
+    public static void main(String[] args) {
+        
+        Person p;
+
+        p = new Person();
+        p.role();
+
+        p = new Employee();
+        p.role();
+
+        p = new Manager();
+        p.role();
+    }
+}
